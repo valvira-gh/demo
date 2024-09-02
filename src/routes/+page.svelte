508ace49e8b524ebafaad45a407eb811a285fdc4
vivapop <valtteri virtanen>
2024-09-03 00:46:02 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Reactive from "./Reactive.svelte";
+
+    let creator = '<strong>vivapop</strong>';
+    let appName = 'Svelte Demo App';
+</script>
+
+
+<header>
+    <h1>Welcome to {appName}!</h1>
+    <div>
+        <p class="subtitle">My first sandbox demo with SvelteKit.</p>
+        <p>Creator: {@html creator}</p>
+    </div>
+</header>
+
+<div class="container">
+    <Reactive />
+</div>
+
+
+<style>
+    header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 2px solid gray;
+
+        && div {
+            display: flex;
+        }
+    }
+
+    .container {
+        display: flex;
+        border: 1px dotted black;
+        justify-content: center;
+    }
+</style>
---
+++ src/routes/Reactive.svelte
@@ -0,0 +1,25 @@
+<script>
+    let count = 0;
+
+    function handleClick() {
+        console.log('clicked')
+        count += 1
+    }
+</script>
+
+<button on:click={handleClick}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+</button>
+
+<style>
+    button {
+        font-size: 1.1rem;
+        font-weight: 500;
+        color: azure;
+        border-radius: .5em;
+        padding: .25em .5em;
+        margin: 2em;
+        background-color: rgb(10, 108, 255);
+    }
+</style>
